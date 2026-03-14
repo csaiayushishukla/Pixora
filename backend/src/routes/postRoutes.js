42fe3e65a04adff776f30ad392e39bd6b9294dc0
csaiayushishukla <2k23.csai1231323@gmail.com>
2026-03-14 22:09:19 +0530

    fix POST route

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,16 +1,25 @@
 ﻿const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
-// GET posts
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// GET POSTS
 router.get("/", async (req, res) => {
   res.json([]);
 });
 
-// POST post
-router.post("/", (req, res) => {
+// CREATE POST
+router.post("/", upload.single("image"), async (req, res) => {
+
+  const caption = req.body.caption;
+
   res.json({
-    message: "POST route working"
+    message: "POST route working",
+    caption: caption
   });
+
 });
 
 module.exports = router;
